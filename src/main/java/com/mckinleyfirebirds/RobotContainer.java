5509cbd76905a7a0aece00ff04b00f2ea7131a95
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2025-02-07 00:07:56 -0500

    Clean up & delete all the useless files

--- src/main/java/com/mckinleyfirebirds/RobotContainer.java
+++ src/main/java/com/mckinleyfirebirds/RobotContainer.java
@@ -5,37 +5,16 @@
 
 package com.mckinleyfirebirds;
 
-import com.mckinleyfirebirds.subsystems.DriveSubsystem;
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.PS4Controller;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
 public class RobotContainer {
-    DriveSubsystem driveSubsystem = new DriveSubsystem();
-    XboxController controller = new XboxController(0);
 
     public RobotContainer() {
         configureBindings();
-        driveSubsystem.setDefaultCommand(
-                new RunCommand(
-                        () -> driveSubsystem.drive(
-                                -MathUtil.applyDeadband(controller.getLeftY(), 0.05),
-                                -MathUtil.applyDeadband(controller.getLeftX(), 0.05),
-                                -MathUtil.applyDeadband(controller.getRightX(), 0.05),
-                                true),
-                        driveSubsystem));
     }
 
     private void configureBindings() {
-        new JoystickButton(controller, PS4Controller.Button.kR1.value)
-                .whileTrue(new RunCommand(
-                        () -> driveSubsystem.setX(),
-                        driveSubsystem
-                ));
+
     }
 
     public Command getAutonomousCommand() {
