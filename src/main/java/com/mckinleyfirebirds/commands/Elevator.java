93c8bd849bb1c2c0f5c409f93335c6c01c396503
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2025-02-06 23:30:35 -0500

    Add the `elevator` command
    It is not the most syntactically pleasing code, but it gets the job done! This code will change in the foreseenable future, and likely it will be turned into a subsystem!

--- src/main/java/com/mckinleyfirebirds/commands/Elevator.java
+++ src/main/java/com/mckinleyfirebirds/commands/Elevator.java
@@ -1,4 +1,27 @@
 package com.mckinleyfirebirds.commands;
 
-public class Elevator {
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Elevator extends Command {
+    SparkMax motor1;
+    SparkMax motor2;
+    boolean direction;
+
+    public Elevator(SparkMax motor1, SparkMax motor2, boolean direction) {
+        this.motor1 = motor1;
+        this.motor2 = motor2;
+        this.direction = direction;
+    }
+    @Override
+    public void execute() {
+        motor1.set(direction ? 0.3 : -0.3);
+        motor2.set(direction ? -0.3 : 0.3);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        motor1.stopMotor();
+        motor2.stopMotor();
+    }
 }
